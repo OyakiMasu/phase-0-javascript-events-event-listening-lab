865ae371f837066c00c7166c181d0555e0390f65
OyakMasu21 <akzaki17@gmail.com>
2022-12-03 20:53:24 +0300

    Completed assignment 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addingEventListener(){ 
+ function addingEventListener(){ 
     var someInput = document.querySelector('input');
 
 someInput.myParam = 'This is my parameter';
@@ -7,4 +7,4 @@ someInput.addEventListener('click', myFunc, false);
 function myFunc(event) {
   window.alert( event.target.myParam );
 }
-}
+} 
